5090f511049453cc8f01a2d26aaeea472e734ae4
tony maggs <acmaggs@gmail.com>
2024-05-07 11:08:34 +0200

    clean

--- comp.sh
+++ comp.sh
@@ -1,1 +1,2 @@
-comm -3 <(find . | sort) <(cd ../crawl/acmaggs.github.io/ ; find . | sort) |grep -v git
+#compare two trees, the first crawled from ../crawl by wget, the second the local files, to find files that are not used
+comm -3 <(find . | sort) <(cd ../crawl/acmaggs.github.io/ ; find . | sort) |grep -v git|grep -v DS_Store
